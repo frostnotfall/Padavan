3a1cf46648c47d731f1fe9e59bf4d1eb0fc754bd
frostnotfall <frostnotfall@gmail.com>
2018-07-18 21:27:45 +0300

    init

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# DESTDIR is your target project's dir
+DESTDIR=/usr/local/src/rt-n56u
+
+ROOTDIR=`pwd`
+
+if [ ! -d "${DESTDIR}" ] ; then
+	echo "Target project doesn't exist! Terminate!!!"
+	exit 1
+fi
+
+cp -fRv "${ROOTDIR}/trunk/" "${DESTDIR}/"
